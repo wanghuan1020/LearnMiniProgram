47fb9116d34f23f67456b09c0bf1fa6fca0096bb
wanghuan <wanghuan@126.com>
2020-04-16 20:33:31 +0800

    页面分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,65 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast() {
+    wx.showToast({
+      title: '加载中',
+      duration: 3000,
+      icon: 'loading',
+      mask: true,
+      // image: "图片路径"
+      success: function () {
+        console.log('展示弹窗成功');
+      },
+      fail: function () {
+        console.log('展示弹窗失败');
+      },
+      complete: function () {
+        console.log('展示弹窗完成');
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal() {
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容',
+      // showCancel: false,
+      cancelText: '退出',
+      cancelColor: 'red',
+      success: function(res) {
+        console.log(res);
+        if(res.cancel) {
+          console.log('用户点击了取消按钮');
+        }
+        if(res.confirm) {
+          console.log('用户点击了确定按钮');
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowLoading() {
+    wx.showLoading({
+      title: '加载ing', 
+      mask: true
+    })
+    setTimeout(()=>{
+      wx.hideLoading()
+    }, 2000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowActionSheet() {
+    wx.showActionSheet({
+      itemList: ['相册', '拍照'],
+      itemColor: 'red',
+      success: function(res) {
+        console.log(res); // tapIndex
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage(options) {
+    console.log(options);
+    return {
+      title: '你好啊，你银河',
+      path: '/pages/about/about',
+      imageUrl: 'http://attach.bbs.miui.com/forum/201401/22/134002dhye9qe2g2nq2geu.jpg'
+    }
   }
 })
