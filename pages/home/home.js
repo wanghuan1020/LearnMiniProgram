003d2c679842d42825ef1ad13191ea1c40991998
wanghuan <wanghuan@126.com>
2020-04-17 17:14:43 +0800

    项目实战

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,20 +1,81 @@
 // pages/home/home.js
+import {
+  getMultiData,
+  getGoodsData
+} from '../../service/home'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    banners: [],
+    recommends: [],
+    titles: ['流行', '新款', '精选'],
+    goods: {
+      pop: {
+        page: 0,
+        list: []
+      },
+      new: {
+        page: 0,
+        list: []
+      },
+      sell: {
+        page: 0,
+        list: []
+      }
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 1.请求轮播图以及推荐数据
+    this._getMuliData()
+    // 2.请求商品数据
+    // this._getGoodsData('pop')
   },
-
+  // -------- 网络请求函数 ---------
+  _getMuliData() {
+    getMultiData().then(res => {
+      // 取出轮播图和推荐的数据
+      const banners = res.data.data.banner.list
+      const recommends = res.data.data.recommend.list
+      // 将banners&recommends房的data中
+      this.setData({
+        banners,
+        recommends
+      })
+    })
+  },
+  _getGoodsData(type) {
+    // 1.获取页码
+    const page = this.data.goods[type].page + 1
+    // 2.发送网络请求
+    getGoodsData(type, page).then(res => {
+      // 2.1.取出数据
+      const { list } = res.data.data
+      // 2.2.将数据设置到对应type的list中
+      const oldList  = this.data.goods[type].list
+      oldList.push(...list)
+      // 2.3.将数据设置到data中的goods中
+      const typeKey = `goods.${type}.list`
+      const pageKey = `goods.${type}.page`
+      this.setData({
+        [typeKey]: oldList,
+        [pageKey]: page
+      })    
+    })
+  },
+  // -------- 事件监听函数 ---------
+  handleTabClick(event) {
+    const {
+      index
+    } = event.detail
+    console.log(index);
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
